47aae7e9be41da842f274e7846e1ad9b49e6d5f0
Suhail Khan <119530622+Fiery-sama@users.noreply.github.com>
2025-05-28 16:58:17 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,8 +3,8 @@ export const About = () => {
         <div className="container mx-auto text-center px-16 py-4">
             <h2 className="text-5xl py-10 text-red-600 font-bold" id="about">About Me</h2>
             <p className="text-lg">
-                I'm Suhail Khan—a full-stack developer with a passion for crafting smart, scalable web solutions. With a toolkit that spans Python, JavaScript, Django, React, and beyond, I thrive at the intersection of logic and creativity. Whether I’m optimizing a MySQL schema, architecting APIs, or collaborating on sleek UI, I bring sharp problem-solving skills and a growth mindset to every sprint. Outside code, you'll find me sketching, gaming, or diving into the next tech trend. I don’t just build apps—I build experiences that work beautifully and scale effortlessly. Let’s make the web smarter, one project at a time.
+                I&apos;m Suhail Khan—a full-stack developer with a passion for crafting smart, scalable web solutions. With a toolkit that spans Python, JavaScript, Django, React, and beyond, I thrive at the intersection of logic and creativity. Whether I&apos;m optimizing a MySQL schema, architecting APIs, or collaborating on sleek UI, I bring sharp problem-solving skills and a growth mindset to every sprint. Outside code, you&apos;ll find me sketching, gaming, or diving into the next tech trend. I don&apos;t just build apps—I build experiences that work beautifully and scale effortlessly. Let&apos;s make the web smarter, one project at a time.
             </p>
-        </div>)
+        </div>
+    )
 }
-
